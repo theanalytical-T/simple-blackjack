edf7ea3e675ba0da8ac2cb7bb311a9546f60f27f
Tiara Brunt <53661450+tiara143@users.noreply.github.com>
2021-10-09 15:54:48 -0500

    10/9/2021
    Starting files on the game blackjack.

---
+++ Blackjack/dist/app.js
@@ -0,0 +1,21 @@
+let firstCard = 5
+let secondCard = 11
+
+let sum = firstCard + secondCard
+// console.log(sum)
+
+if (sum < 21) {
+    console.log("Do you want to draw a new card? 😊")
+} else if (sum === 21) {
+    console.log("Yay! You've got Blackjack! 🥳")
+} else {
+    console.log("You're out of the game! 😭")
+}
+
+// let age = 22
+
+// if (age <= 20) {
+//     console.log("You can not enter the club!")
+// } else {
+//     console.log("Welcome!")
+// }
